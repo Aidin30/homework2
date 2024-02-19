388c2d2113b63bcdabd34b9f486608fd2762a283
Aidin30 <azamatuuluaidin@gmail.com>
2024-02-19 15:16:26 +0600

    first commit

---
+++ bin/dart_one.dart
@@ -0,0 +1,5 @@
+import 'package:dart_one/dart_one.dart' as dart_one;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_one.calculate()}!');
+}
---
+++ test/dart_one_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_one/dart_one.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
